f555bf2ba01cf2131dfe41ccd0d5f0f9d6cf97b2
Ahmet Akgul <akgulahmet@outlook.com>
2017-07-25 21:54:00 -0400

    Change api request, list titles into array

--- wikipedia.js
+++ wikipedia.js
@@ -1,19 +1,28 @@
 $(document).ready(function(){
   $("#searchbutton").click(function(){
-    console.log($("#search").val());
+
     $.ajax({
-      url:"https://en.wikipedia.org/w/api.php?action=query&format=json&prop=&list=search&meta=&rawcontinue=1&titles=",
-      data:"srsearch="+ $("#search").val(),
+      url:"https://en.wikipedia.org/w/api.php?action=query&format=json&prop=links%7Cextracts%7Cinfo&rawcontinue=1&generator=search&exsentences=2&exintro=1&explaintext=1&inprop=url",
+      data:"gsrsearch="+ $("#search").val(),
       dataType:"json",
       header:{
         contentType:"application/javascript",
         Accept:"application/JSON"
       },
       success: function(data){
-        $()
-        console.log($("#search").val());
+        var titles = [];
+        for (var pageId in data.query.pages){
+          if (data.query.pages.hasOwnProperty(pageId)){
+            titles.splice(data.query.pages[pageId].index, 0, data.query.pages[pageId].title);
+            console.log(data.query.pages[pageId].title);
+            console.log(titles.join());
+          }
+        }
+        // $("#media1 h4").html(data.query.pages[0].title);
+        // console.log(data.query.pages[0].title);
+
         console.log(data);
-        console.log("Hello");
+
         console.log(this.url);
       },
       cache:true
