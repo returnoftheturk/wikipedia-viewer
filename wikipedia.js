79fee8d423b1998f3c9bbefbd4d02837e6dd8fc3
Ahmet Akgul <akgulahmet@outlook.com>
2017-07-25 20:04:57 -0400

    Correctly poll API given user input in the search function.  Have yet to display results.

--- wikipedia.js
+++ wikipedia.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){
   $("#searchbutton").click(function(){
     console.log($("#search").val());
-  })
+    $.ajax({
+      url:"https://en.wikipedia.org/w/api.php?action=query&format=json&prop=&list=search&meta=&rawcontinue=1&titles=",
+      data:"srsearch="+ $("#search").val(),
+      dataType:"json",
+      header:{
+        contentType:"application/javascript",
+        Accept:"application/JSON"
+      },
+      success: function(data){
+        $()
+        console.log($("#search").val());
+        console.log(data);
+        console.log("Hello");
+        console.log(this.url);
+      },
+      cache:true
+    });
+  });
+
+
 });
