ee720f39f91e7583c2f1a2da60cb13b44a0c16b4
Ahmet Akgul <akgualhmet@Outlook.com>
2017-07-27 14:46:06 -0400

    Create html div element to use to populate the results.  It is a div with a title and body and link to external wikipedia page

--- wikipedia.js
+++ wikipedia.js
@@ -11,13 +11,22 @@ $(document).ready(function(){
       },
       success: function(data){
         var titles = [];
+        var bodyContent = [];
+        var links = [];
+        
+
         for (var pageId in data.query.pages){
           if (data.query.pages.hasOwnProperty(pageId)){
             titles.splice(data.query.pages[pageId].index, 0, data.query.pages[pageId].title);
-            console.log(data.query.pages[pageId].title);
-            console.log(titles.join());
+            bodyContent.splice(data.query.pages[pageId].index, 0, data.query.pages[pageId].extract);
+            links.splice(data.query.pages[pageId].index, 0, data.query.pages[pageId].fullurl);
+            // console.log(data.query.pages[pageId].title);
           }
         }
+
+        console.log(titles.join());
+        console.log(bodyContent.join());
+        console.log(links.join());
         // $("#media1 h4").html(data.query.pages[0].title);
         // console.log(data.query.pages[0].title);
 
